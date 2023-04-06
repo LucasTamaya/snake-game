9e2a3fa01e47d109ae844b551b16c27268175d55
LucasTamaya <lucastamaya.974@gmail.com>
2023-04-06 16:31:40 +0400

    make sure the snake doesn't go out of the window

--- src/events/events.c
+++ src/events/events.c
@@ -1,26 +1,55 @@
 #include "events.h"
 
+void moveSnakeUp();
+void moveSnakeDown();
+void moveSnakeLeft();
+void moveSnakeRight();
+
 void handleKeydownEvents(SDL_Event *event)
 {
     switch (event->key.keysym.sym)
     {
     case SDLK_UP:
-        snake.y -= 10;
+        moveSnakeUp();
         break;
 
     case SDLK_DOWN:
-        snake.y += 10;
+        moveSnakeDown();
         break;
 
     case SDLK_LEFT:
-        snake.x -= 10;
+        moveSnakeLeft();
         break;
 
     case SDLK_RIGHT:
-        snake.x += 10;
+        moveSnakeRight();
         break;
 
     default:
         break;
     }
+}
+
+void moveSnakeUp()
+{
+    if (snake.y > 0)
+        snake.y -= 10;
+}
+
+void moveSnakeDown()
+{
+    if (snake.y < WINDOW_H - SNAKE_SIZE)
+        snake.y += 10;
+}
+
+void moveSnakeLeft()
+{
+    if (snake.x > 0)
+        snake.x -= 10;
+}
+
+void moveSnakeRight()
+{
+    if (snake.x < WINDOW_W - SNAKE_SIZE)
+        snake.x += 10;
 }
